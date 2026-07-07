566404dfe8477598b6477348bbf92a9ce3ccbc84
Alexandru Dediu <alexdediu98@gmail.com>
2026-07-07 15:34:23 +0300

    Add test for locked out user login

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,13 +1,22 @@
 from playwright.sync_api import sync_playwright
-
+from pages.login_page import LoginPage
 
 def test_login_cu_credentiale_valide():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto("https://www.saucedemo.com")
-        page.fill("[data-test='username']", "standard_user")
-        page.fill("[data-test='password']", "secret_sauce")
-        page.click("[data-test='login-button']")
+        login_page = LoginPage(page)
+        login_page.login("standard_user", "secret_sauce")
         titlu = page.inner_text("[data-test='title']")
         assert titlu == "Products"
+        
+def test_login_cu_utilizator_blocat():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.saucedemo.com")
+        login_page = LoginPage(page)
+        login_page.login("locked_out_user", "secret_sauce")
+        mesaj_eroare = page.inner_text("[data-test='error']")
+        assert "locked out" in mesaj_eroare
